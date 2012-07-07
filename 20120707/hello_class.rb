e41d63107e0f13f3e56568a6d4693bb8c4baedfa
hidehigo <hidehigo@gmail.com>
2012-07-07 12:20:37 +0900

    class変数。これは使わない方がいいと思ふ

--- 20120707/hello_class.rb
+++ 20120707/hello_class.rb
@@ -1,12 +1,14 @@
 class HelloWorld
   Version = "1.0"
   attr_accessor :name
+  @@count = 0
 
   def initialize(myname="Ruby")
     @name = myname
   end
 
   def hello
+    @@count += 1
     print "Hello, world. I am ", self.name, ".\n"
   end
 
@@ -18,6 +20,10 @@ class HelloWorld
     def hello(name)
       print name, " said hello.\n"
     end
+
+    def count
+      @@count
+    end
   end
 end
 
@@ -25,10 +31,12 @@ bob = HelloWorld.new("Bob")
 alice = HelloWorld.new("Alice")
 ruby = HelloWorld.new
 
+p HelloWorld.count
 bob.hello
 alice.hello
 ruby.hello
 
+p HelloWorld.count
 p bob.name
 bob.name = "hide"
 p bob.name
@@ -36,4 +44,6 @@ bob.hello
 bob.oreore
 bob.hello
 
+p HelloWorld.count
 HelloWorld.hello("John")
+p HelloWorld.count
