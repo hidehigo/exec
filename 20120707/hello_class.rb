007a0b7b61df41b6980b1cac09487f90fb03ddcd
hidehigo <hidehigo@gmail.com>
2012-07-07 11:46:55 +0900

    setterはname=のように定義できる

---
+++ 20120707/hello_class.rb
@@ -0,0 +1,33 @@
+class HelloWorld
+  Version = "1.0"
+
+  def initialize(myname="Ruby")
+    @name = myname
+  end
+
+  def hello
+    print "Hello, world. I am ", @name, ".\n"
+  end
+
+  def name
+    return @name
+  end
+
+  def name=(value)
+    @name = value
+  end
+
+end
+
+bob = HelloWorld.new("Bob")
+alice = HelloWorld.new("Alice")
+ruby = HelloWorld.new
+
+bob.hello
+alice.hello
+ruby.hello
+
+p bob.name
+bob.name = "hide"
+p bob.name
+bob.hello
