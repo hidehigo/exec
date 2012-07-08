d3b4b0e0cd02aece7a8e68a308517e97c9144706
hidehigo <hidehigo@gmail.com>
2012-07-09 07:57:04 +0900

    まとめて表記

--- 20120709/acc_test.rb
+++ 20120709/acc_test.rb
@@ -1,13 +1,14 @@
 class AccTest
+
+  public
   def pub
     puts "pub is a public method"
   end
-  public :pub
 
+  private
   def priv
     puts "priv is a pivate method"
   end
-  private :priv
 end
 
 acc = AccTest.new
