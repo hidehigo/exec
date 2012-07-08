afde8821b6ae5785731812ec2ed5d28395158ad3
hidehigo <hidehigo@gmail.com>
2012-07-09 08:44:37 +0900

    Mix-inごにょごにょしてみる

--- 20120709/hello_module.rb
+++ 20120709/hello_module.rb
@@ -6,9 +6,34 @@ module HelloModule
   module_function :hello
 end
 
+module FooModule
+  Version = "1.0"
+  def fuga
+    p "fugafuga"
+  end
+  def foo
+    p self # Mix-inされるとselfはclassになる
+  end
+  #module_function :foo
+end
+
 p HelloModule::Version
 HelloModule.hello("Alice")
 
 include HelloModule
 p Version
 hello("hoge")
+
+#FooModule.foo # module_fuctionで公開してなかったらNoMethodError
+
+class MixInTest
+  Version = "1.0"
+  def fuga
+    p "fuga"
+  end
+  include FooModule
+end
+
+a = MixInTest.new
+a.foo
+a.fuga # moduleよりもclass内が優先(そりゃそうか)
