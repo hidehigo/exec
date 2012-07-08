811e9dd8d3aacbc7265464b19331f554bb1fa5cb
hidehigo <hidehigo@gmail.com>
2012-07-09 08:29:38 +0900

    module

---
+++ 20120709/hello_module.rb
@@ -0,0 +1,14 @@
+module HelloModule
+  Version = "1.0"
+  def hello(name)
+    print "Hello, ", name, ".\n"
+  end
+  module_function :hello
+end
+
+p HelloModule::Version
+HelloModule.hello("Alice")
+
+include HelloModule
+p Version
+hello("hoge")
