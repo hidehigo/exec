6afce134461a380062cf291ba042cce3335a6358
hidehigo <hidehigo@gmail.com>
2012-07-13 08:20:14 +0900

    10章finish

---
+++ 20120713/practice.rb
@@ -0,0 +1,23 @@
+ary = []
+10.times do |i|
+  #ary.push(i)
+  ary << i
+end
+p ary
+
+#---
+ary = []
+10.times { |i| ary << i }
+puts ary
+
+#---
+ary = []
+0.upto(10) { |i| ary << i }
+p ary
+
+#--- euler prob.1
+sum = 0
+1.upto(1000){ |i| sum+=i if((i%3).zero? || (i%5).zero?) }
+p sum
+
+
