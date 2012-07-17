7afafffa52cdb23d975b373f3e0d837a89472d7c
hidehigo <hidehigo@gmail.com>
2012-07-18 08:50:38 +0900

    symbolまだよーわからん

--- 20120718/hash1.rb
+++ 20120718/hash1.rb
@@ -2,3 +2,12 @@ h1 = Hash.new
 h2 = Hash.new("")
 p h1[:not_exist]
 p h2[:not_exist]
+
+h3 = {"key" => "hoge", :key => "hage"}
+p h3["key"]
+p h3[:key]
+
+h4 = {key: "hoge"}
+# h4 = {key : "hoge"} <=error
+p h4["key"]
+p h4[:key]
