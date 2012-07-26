43b6ce210735950fbeed7cb1fa8b47e0999812a9
hidehigo <hidehigo@gmail.com>
2012-07-27 08:10:55 +0900

    euler2 injectで書いてみた

---
+++ 20120727/euler_2.rb
@@ -0,0 +1,12 @@
+first,second = [ 1, 2 ]
+max = 4.0e6.to_i
+fibo = Array.new
+fibo << first
+fibo << second
+loop do 
+  next_elem = fibo[-1] + fibo[-2]
+  break if next_elem > max
+  fibo << next_elem
+end
+p fibo
+p fibo.inject(0){|sum, elem| if (elem%2).zero? then sum + elem else sum end}
