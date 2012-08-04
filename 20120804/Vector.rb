a0b3c49ac191072396028281cff08018dd1976b9
hidehigo <hidehigo@gmail.com>
2012-08-04 09:47:55 +0900

    Vector写経3

--- 20120804/Vector.rb
+++ 20120804/Vector.rb
@@ -21,12 +21,32 @@ class Vector
   def ~@
     Vector.new(-@y, -@x)
   end
+  def [](idx)
+    case idx
+    when 0
+      @x
+    when 1
+      @y
+    else
+      raise ArgumentError, "out of range `#{idx}'"
+    end
+  end
+  def []=(idx, val)
+    case idx
+    when 0
+      @x = val
+    when 1
+      @y = val
+    else
+      raise ArgumentError, "out of range `#{idx}'"
+    end
+  end
 
 end
 
 vec0 = Vector.new(3, 6)
 
-p vec0
-p +vec0
-p -vec0
-p ~vec0
+p vec0[0]
+p vec0[1] = 2
+p vec0[1]
+p vec0[2]
