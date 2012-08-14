0037a2c293ad5ac237ca147670b3aa8dbe5096e4
hidehigo <hidehigo@gmail.com>
2012-08-14 14:56:30 +0900

    refactor 002.rb

--- ProjectEuler/002.rb
+++ ProjectEuler/002.rb
@@ -1,4 +1,10 @@
 ary = [1,2]
-nex = ary[-1] + ary[-2]
-while nex <= 4000000; ary << nex; nex = ary[-1] + ary[-2]; end
-p ary.inject(0){|sum, i| (i%2).zero? ? sum + i : sum}
+nextnum = ary[-1] + ary[-2]
+sum = 2 #loopは2の次から始まるので。
+loop do 
+  break if nextnum > 4000000
+  sum += nextnum if (nextnum % 2).zero?
+  ary << nextnum
+  nextnum = ary[-1] + ary[-2]
+end
+p sum
