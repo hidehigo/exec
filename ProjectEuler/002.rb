dec403ca7e56808dc6b638d65a01e172e2547ac2
hidehigo <hidehigo@gmail.com>
2012-08-14 14:55:55 +0900

    refactor 002.rb

--- ProjectEuler/002.rb
+++ ProjectEuler/002.rb
@@ -1,20 +1,12 @@
-
-first = 1
-second = 2
-finish = 4000000
-
+first,second = [ 1, 2 ]
+max = 4.0e6.to_i
 fibo = Array.new
 fibo << first
 fibo << second
-
-sum_even = 0
-
-loop do
-  break if fibo[-1] + fibo[-2] > finish
-  fibo.push (fibo[-1] + fibo[-2])
-  sum_even += fibo[-1] if (fibo[-1] % 2).zero?
+loop do 
+  next_elem = fibo[-1] + fibo[-2]
+  break if next_elem > max
+  fibo << next_elem
 end
-
 p fibo
-p sum_even + 2
-#p fibo.inject(0){ |sum, elem| sum + elem if (elem % 2).zero? }
+p fibo.inject(0){|sum, elem| (elem%2).zero? ? sum + elem : sum}
