de173600c9cf96cafef3db0ae929af2ac1fbc6c0
hidehigo <hidehigo@gmail.com>
2012-08-16 00:02:17 +0900

    solved euler012 ようやく完了できるスピードに。13sくらい

--- ProjectEuler/012.rb
+++ ProjectEuler/012.rb
@@ -1,25 +1,34 @@
 start = Time.now
-
+# 約数の数は素因数分解した組み合わせ数
+# n = 2^2 * 3^2 * 5^1 * 7*1
+#   => (2+1) * (2+1) * (1+1) * (1+1)
 now = 0
 idx = 1
 $loop_cnt = 0
 
-def factorize(n)
-  i = 1
-  ary = []
-  while n.to_f/i >= 2
-    ary << i if (n%i).zero?
-    i += 1
+def factorial_hash(num)
+  factors = Hash.new(0)
+  divisor = 2 # start from 2
+  divided = num.to_i
+  while ( divisor <= divided ) do
     $loop_cnt += 1
+    if (divided % divisor).zero? then
+      factors[divisor] += 1
+      divided = divided / divisor
+      next
+    end
+    divisor += 1
   end
-  ary << n
-  return ary
+  return factors
 end
 
 loop do 
   now += idx
-  p ( a = factorize(now))
-  break if a.size > 500
+  hash = factorial_hash(now)
+  #p hash
+  factors_num = hash.inject(1){|prod, (key, val)| prod *= (val + 1)}
+  #p factors_num
+  break if factors_num > 500
   idx += 1
 end
 
