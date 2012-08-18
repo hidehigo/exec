cd3b06bca6ef308859f4c20607d07d94f20cd522
hidehigo <hidehigo@gmail.com>
2012-08-18 16:35:14 +0900

    014.rb hashで書くよう修正

--- ProjectEuler/014.rb
+++ ProjectEuler/014.rb
@@ -1,25 +1,26 @@
 start = Time.now
-@a = [nil, 0]
+@a = Hash.new(0)
+@a[1] = 1
 
 def calc(n) 
-  return @a[n] if ! @a[n].nil?
+  return @a[n] if @a[n] != 0
   m = n.even? ? n/2 : 3 * n + 1
-  if ! @a[m].nil?
+  if @a[m] != 0
     @a[n] = @a[m] + 1
   else
     @a[n] = calc(m) + 1
   end
   # nになる数も埋めておく
   if n.even?
-    if @a[ o = 2*n ].nil? then @a[o] = @a[n] + 1 end
+    if @a[ o = 2*n ] == 0 then @a[o] = @a[n] + 1 end
   else
-    if ( (n-1) % 3 ) == 0 && @a[ o = (n-1)/3 ].nil? then @a[o] = @a[n] + 1 end
+    if ( (n-1) % 3 ) == 0 && @a[ o = (n-1)/3 ] == 0 then @a[o] = @a[n] + 1 end
   end
   return @a[n]
 end
 
-(1..100).each{|n|
-  if @a[n] 
+(1..10000).each{|n|
+  if @a[n] != 0 
     #puts "exists:#{n}"
     next
   else
@@ -29,8 +30,9 @@ end
   end
 }
 
-puts "max_index: " + @a.size.to_s
-puts "max_step: " + step = @a.inject(0){|max,n| if n.nil? then max else max < n ? n : max end}.to_s
-puts "num of not nil: " + @a.select{|n| !n.nil? }.size.to_s
+puts "max_index: " + @a.inject(0){|max,(k,v)| max < k ? k : max }.to_s
+puts "max_step: " + step = @a.inject(0){|max,(k,v)| max < v ? v : max }.to_s
+puts "num of not nil: " + @a.size.to_s
 puts "max_at: " + @a.index(step.to_i).to_s
+print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
 #@a.each_index{|n| print "#{n}:#{@a[n]} "}
