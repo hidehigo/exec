19922e17d3b7e812d57f5c557ab942144aa2e5ba
hidehigo <hidehigo@gmail.com>
2012-08-18 13:05:29 +0900

    014.rb 修正

--- ProjectEuler/014.rb
+++ ProjectEuler/014.rb
@@ -11,26 +11,26 @@ def calc(n)
   end
   # nになる数も埋めておく
   if n.even?
-    if @a[o = 2*n].nil? then @a[o] = @a[n] + 1 end
+    if @a[ o = 2*n ].nil? then @a[o] = @a[n] + 1 end
   else
-    if ( o = (n - 1) % 3 ) == 0 && @a[0].nil? then @a[o] = @a[n] + 1 end
+    if ( (n-1) % 3 ) == 0 && @a[ o = (n-1)/3 ].nil? then @a[o] = @a[n] + 1 end
   end
   return @a[n]
 end
 
-(1..10000).each{|n|
+(1..100).each{|n|
   if @a[n] 
     #puts "exists:#{n}"
     next
   else
-    puts "calc:#{n}"
+    #puts "calc:#{n}"
     calc(n)
-    print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
+    #print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
   end
 }
 
 puts "max_index: " + @a.size.to_s
 puts "max_step: " + step = @a.inject(0){|max,n| if n.nil? then max else max < n ? n : max end}.to_s
 puts "num of not nil: " + @a.select{|n| !n.nil? }.size.to_s
-puts "max_value: " + @a.index(step.to_i).to_s
+puts "max_at: " + @a.index(step.to_i).to_s
 #@a.each_index{|n| print "#{n}:#{@a[n]} "}
