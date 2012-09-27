7948dfe703e34e8ad8d46956e864fdbe29a2abf3
hidehigo <hidehigo@gmail.com>
2012-09-27 22:15:40 +0900

    solved yapc2012 prime wars

---
+++ ProjectEuler/yapc.pl
@@ -0,0 +1,59 @@
+#/usr/bin/perl
+use strict;
+
+#start = Time.now
+my @primes = (2, 3, 5, 7);
+my $loop_cnt = 0;
+my $loop_cnt2 = 0;
+my $loop_cnt3 = 0;
+my $sum = 2 + 3 + 5 + 7;
+
+## 素数の候補を返す。30(2*3*5)で割った余りが以下のものが素数の候補
+my @pc = (1, 7, 11, 13, 17, 19, 23, 29,
+       31); #←繰り上がり対応用
+
+sub next_prime_candidate {
+  my $now = shift;
+  my $mod = $now % 30;
+  my $idx;
+  for ( my $i=0; $i<=@pc; $i++) {
+    if ( $pc[$i] == $mod ) {
+      $idx = $i;
+      last;
+    }
+  }
+  return $now - $mod + $pc[$idx + 1]
+}
+
+sub is_prime {
+  my $n = shift;
+  foreach my $p (@primes) {
+    $loop_cnt ++;
+    return 0 unless $n % $p;
+  }
+  for (my $i = $primes[-1]; $i < $n; $i ++) {
+    $loop_cnt2 ++;
+    return 1 if $i * $i > $n;
+    return 0 unless $n % $i;
+  }
+  return 0;
+}
+
+my $n = 8;
+while (@primes < 10000) {
+  $loop_cnt3 ++;
+  if ( is_prime($n) ) {
+    push(@primes, $n);
+    $sum += $n;
+    $n = next_prime_candidate($n);
+    next;
+  }
+  $n ++;
+}
+
+#print $primes[-1]."\n";
+print $sum."\n";
+#print "loop_cnt: $loop_cnt\n";
+#print "loop_cnt: $loop_cnt2\n";
+#print "loop_cnt: $loop_cnt3\n";
+#print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
---
+++ ProjectEuler/yapc.rb
@@ -0,0 +1,48 @@
+#/usr/bin/ruby
+start = Time.now
+@primes = [2, 3, 5, 7]
+@loop_cnt = 0
+@loop_cnt2 = 0
+@loop_cnt3 = 0
+@sum = @primes.inject(:+)
+
+## 素数の候補を返す。30(2*3*5)で割った余りが以下のものが素数の候補
+@pc = [1, 7, 11, 13, 17, 19, 23, 29, \
+       31] #←繰り上がり対応用
+def next_prime_candidate(now)
+  mod = now % 30
+  return now - mod + @pc[ @pc.index(mod) + 1 ]
+end
+
+def prime?(n)
+  @primes.each do |prime|
+    @loop_cnt += 1
+    return false if n % prime  == 0
+  end
+  (@primes[-1] .. n ).each do |m|
+    @loop_cnt2 += 1
+    return true if m ** 2 > n
+    return false if n % m == 0
+  end
+  return false
+end
+
+n = 8
+while @primes.size < 10000
+  @loop_cnt3 += 1
+  if prime?(n)
+    @primes << n
+    @sum += n
+    n = next_prime_candidate(n)
+    #n += 1
+    next
+  end
+  n += 1
+end
+
+#p @primes[-1]
+p @sum
+#print "loop_cnt: #{@loop_cnt}\n"
+#print "loop_cnt: #{@loop_cnt2}\n"
+#print "loop_cnt: #{@loop_cnt3}\n"
+#print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
