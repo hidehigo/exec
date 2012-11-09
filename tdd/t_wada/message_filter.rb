85524d211782b609bad405106c8ec9156ca8c23b
hidehigo <hidehigo@gmail.com>
2012-11-09 09:09:08 +0900

    rpec_tdd step2:仮実装

--- tdd/t_wada/message_filter.rb
+++ tdd/t_wada/message_filter.rb
@@ -1,2 +1,8 @@
 class MessageFilter
+  def initialize(word)
+    @word = word
+  end
+  def detect?(text)
+    return true
+  end
 end
--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -3,4 +3,8 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+  it 'shold detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello from foo').should == true
+  end
 end
