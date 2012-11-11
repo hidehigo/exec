50dd6c705644880ea02c802e1ac259135a49a14f
hidehigo <hidehigo@gmail.com>
2012-11-11 12:18:35 +0900

    refactor test

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -3,14 +3,7 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter, 'with augument "foo"' do
-  before() do
-    @filter = MessageFilter.new('foo')
-  end
-  subject { @filter }
-  it {
-    should be_detect('hello from foo')
-  }
-  it {
-    should_not be_detect('hello, world!')
-  }
+  subject { MessageFilter.new('foo') }
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello, world!') }
 end
