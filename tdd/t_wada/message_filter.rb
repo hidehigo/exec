c27a6fc6cb6f02663ada73824e351149d60ca87b
hidehigo <hidehigo@gmail.com>
2012-11-11 12:47:01 +0900

    added code

--- tdd/t_wada/message_filter.rb
+++ tdd/t_wada/message_filter.rb
@@ -1,8 +1,10 @@
 class MessageFilter
   def initialize(*words)
-    @words = words
+    @ng_words = words
   end
+  attr_reader :ng_words
+
   def detect?(text)
-    @words.any?{|w| text.include?(w)}
+    @ng_words.any?{|w| text.include?(w)}
   end
 end
