721d04c3c97c8d7516210e0c015d956e30381d21
hidehigo <hidehigo@gmail.com>
2012-11-11 12:14:11 +0900

    itの記述を変更

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -6,10 +6,10 @@ describe MessageFilter do
   before() do
     @filter = MessageFilter.new('foo')
   end
-  it 'shold detect message with NG word' do
+  it {
     @filter.should be_detect('hello from foo')
-  end
-  it 'shold not detect message without NG word' do
+  }
+  it {
     @filter.should_not be_detect('hello, world!')
-  end
+  }
 end
