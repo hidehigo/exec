7dd16ca331722891e543f5924343af3d14af3fd4
hidehigo <hidehigo@gmail.com>
2012-11-11 11:49:50 +0900

    refator test beforeを括る

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -3,12 +3,13 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+  before(:each) do
+    @filter = MessageFilter.new('foo')
+  end
   it 'shold detect message with NG word' do
-    filter = MessageFilter.new('foo')
-    filter.detect?('hello from foo').should == true
+    @filter.detect?('hello from foo').should == true
   end
   it 'shold not detect message without NG word' do
-    filter = MessageFilter.new('foo')
-    filter.detect?('hello, world!').should == false
+    @filter.detect?('hello, world!').should == false
   end
 end
