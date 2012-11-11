545241595907ae623b860dcfd9067d90a6a08273
hidehigo <hidehigo@gmail.com>
2012-11-11 12:55:23 +0900

    refactor test

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -6,12 +6,12 @@ describe MessageFilter do
   share_examples_for 'MessageFilter with argument "foo"' do
     it { should be_detect('hello from foo') }
     it { should_not be_detect('hello, world!') }
+    its(:ng_words) { should_not be_empty }
   end
 
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_should_behave_like 'MessageFilter with argument "foo"'
-    its(:ng_words) { should_not be_empty }
   end
 
   context 'with argument "foo","bar"' do
