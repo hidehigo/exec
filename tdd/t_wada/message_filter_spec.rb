7687dfb12ce2b80a764c58b1d3b9c474032c706d
hidehigo <hidehigo@gmail.com>
2012-11-11 12:36:44 +0900

    refactor tests

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -2,18 +2,20 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
-share_examples_for 'MessageFilter with argument "foo"' do
-  it { should be_detect('hello from foo') }
-  it { should_not be_detect('hello, world!') }
-end
+describe MessageFilter do
+  share_examples_for 'MessageFilter with argument "foo"' do
+    it { should be_detect('hello from foo') }
+    it { should_not be_detect('hello, world!') }
+  end
 
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
-  it_should_behave_like 'MessageFilter with argument "foo"'
-end
+  context 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+  end
 
-describe MessageFilter, 'with argument "foo","bar"' do
-  subject { MessageFilter.new('foo','bar') }
-  it { should be_detect('hello from bar') }
-  it_should_behave_like 'MessageFilter with argument "foo"'
+  context 'with argument "foo","bar"' do
+    subject { MessageFilter.new('foo','bar') }
+    it { should be_detect('hello from bar') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+  end
 end
