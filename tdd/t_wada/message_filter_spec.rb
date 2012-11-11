9672067e3dbee037c00b0a6e2bc899f35b3cde34
hidehigo <hidehigo@gmail.com>
2012-11-11 12:32:55 +0900

    refactor tests

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -2,15 +2,18 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
+share_examples_for 'MessageFilter with argument "foo"' do
   it { should be_detect('hello from foo') }
   it { should_not be_detect('hello, world!') }
+end
+
+describe MessageFilter, 'with argument "foo"' do
+  subject { MessageFilter.new('foo') }
+  it_should_behave_like 'MessageFilter with argument "foo"'
 end
 
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo','bar') }
   it { should be_detect('hello from bar') }
-  it { should be_detect('hello from foo') }
-  it { should_not be_detect('hello, world!') }
+  it_should_behave_like 'MessageFilter with argument "foo"'
 end
