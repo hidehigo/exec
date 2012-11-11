c9a27422a6d3e4c73c78331dcfc8eda9fee70e52
hidehigo <hidehigo@gmail.com>
2012-11-11 12:29:06 +0900

    added tests

--- tdd/t_wada/message_filter_spec.rb
+++ tdd/t_wada/message_filter_spec.rb
@@ -11,4 +11,6 @@ end
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo','bar') }
   it { should be_detect('hello from bar') }
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello, world!') }
 end
