ca223c54ea470e346bf589df6a4ff66fecc11cd2
hidehigo <hidehigo@gmail.com>
2012-11-13 22:56:02 +0900

    refactor test ＊リファクタも安心ｗ　letはちょっとしっくりきてない。そろそろsymbolの理解が必要か

--- tdd/rspec_book/spec/codebreaker/game_spec.rb
+++ tdd/rspec_book/spec/codebreaker/game_spec.rb
@@ -3,19 +3,17 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
-      before(:each) do
-        @output = double('output').as_null_object
-        @game = Game.new(@output)
-      end
+      let(:output) { double('output').as_null_object }
+      let(:game)   { Game.new(output) }
 
       it "sends a welcome message" do
-        @output.should_receive(:puts).with('Welcome to Codebreaker!')
-        @game.start
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+        game.start
       end
 
       it "prompts for the first guess" do 
-        @output.should_receive(:puts).with('Enter guess:')
-        @game.start
+        output.should_receive(:puts).with('Enter guess:')
+        game.start
       end
     end
   end
