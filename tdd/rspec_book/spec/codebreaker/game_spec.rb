d372d3f478f97e0b21045df33fd92bdede573123
hidehigo <hidehigo@gmail.com>
2012-11-13 22:52:41 +0900

    refactor test ＊itの中での実行順は保証されてない？評価するのがitだから？

--- tdd/rspec_book/spec/codebreaker/game_spec.rb
+++ tdd/rspec_book/spec/codebreaker/game_spec.rb
@@ -3,22 +3,19 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
+      before(:each) do
+        @output = double('output').as_null_object
+        @game = Game.new(@output)
+      end
+
       it "sends a welcome message" do
-        output = double('output').as_null_object
-        game = Game.new(output)
-
-        output.should_receive(:puts).with('Welcome to Codebreaker!')
-
-        game.start
+        @output.should_receive(:puts).with('Welcome to Codebreaker!')
+        @game.start
       end
 
       it "prompts for the first guess" do 
-        output = double('output').as_null_object
-        game = Game.new(output)
-
-        output.should_receive(:puts).with('Enter guess:')
-
-        game.start
+        @output.should_receive(:puts).with('Enter guess:')
+        @game.start
       end
     end
   end
