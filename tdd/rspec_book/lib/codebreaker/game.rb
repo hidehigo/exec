3d430417d909a7c501311e13c257de52cdcc363e
hidehigo <hidehigo@gmail.com>
2012-11-14 09:10:20 +0900

    RSpecBook: 7.3

--- tdd/rspec_book/lib/codebreaker/game.rb
+++ tdd/rspec_book/lib/codebreaker/game.rb
@@ -11,14 +11,26 @@ module Codebreaker
     end
 
     def guess(guess)
-      if guess[0] == @secret[0]
-        mark = '+'
-      elsif @secret.include?(guess[0])
-        mark = '-'
-      else
-        mark = ''
+      mark = ''
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          mark << '+'
+        end
+      end
+      (0..3).each do |index|
+        if number_match?(guess, index)
+          mark << '-'
+        end
       end
       @output.puts mark
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
   end
 end
--- tdd/rspec_book/spec/codebreaker/game_spec.rb
+++ tdd/rspec_book/spec/codebreaker/game_spec.rb
@@ -39,6 +39,20 @@ module Codebreaker
           game.guess('1555')
         end
       end
+      context "with 2 number matches" do
+        it "sends mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+      context "with 1 number match and 1 exact match (in that order)" do
+        it "sends mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
     end
   end
 end
