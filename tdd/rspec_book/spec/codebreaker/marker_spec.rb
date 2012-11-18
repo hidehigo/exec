208f2c66e79f59e721535c62c03b1b82ac001dbc
hidehigo <hidehigo@gmail.com>
2012-11-18 10:39:43 +0900

    RSpecBook: 9.1

--- tdd/rspec_book/spec/codebreaker/marker_spec.rb
+++ tdd/rspec_book/spec/codebreaker/marker_spec.rb
@@ -58,6 +58,15 @@ module Codebreaker
         end
       end
     end
+
+    describe "#number_match_count" do
+      context "with 1 exact match duplicated in guess" do
+        it "returns 0" do
+          marker = Marker.new('1234', '1155')
+          marker.number_match_count.should == 0
+        end
+      end
+    end
   end
 end
 
