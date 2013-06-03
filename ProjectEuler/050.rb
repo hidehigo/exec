4f3daf06a9b418b5383a396ca6b6b2be393d7936
hidehigo <hidehigo@gmail.com>
2013-06-03 09:07:35 +0900

    改善。なんとか解けるが15minかかるｗ

--- ProjectEuler/050.rb
+++ ProjectEuler/050.rb
@@ -1,28 +1,38 @@
 require 'prime'
-@limit = 1_000
+start = Time.now
+
+@limit = 1_000_000
 primes = Prime.each(@limit).to_a
 answers = Array.new
 size = primes.size
-catch(:end) do
+tmp_max = 0
+#catch(:end) do
 primes.each_index do |i|
-  (i+1 .. size-1).to_a.reverse.each do |j|
-    sum = primes[i..j].inject(:+)
-    next if sum > @limit
-    if sum.prime?
-      p [ primes[i..j].to_s, sum ]
-      answers.push(primes[i..j])
+  tmp_sum = primes[i..-1].inject(:+)
+  for j in (1 .. size-i)
+    #p [i, j, primes[i..-j]]
+    break if (size - i - j) < tmp_max #tmp_maxより短くなったら調べない
+    tmp_sum -= primes[-j]
+    next if tmp_sum > @limit
+    if tmp_sum.prime?
+      tmp_max = size - i - j
+      #p [ primes[i..-(j+1)].to_s, tmp_sum, tmp_max ]
+      answers.push(primes[i..-(j+1)])
       break
     end
   end
 end
-end
+#end
 
-max_prime = 1
-max_length = 1
-answers.each do |a|
-  if a.size > max_length
-    max_length = a.size
-    max_prime = a.inject(:+)
-  end
-end
-p max_prime
+#max_prime = 1
+#max_length = 1
+#answers.each do |a|
+#  if a.size > max_length
+#    max_length = a.size
+#    max_prime = a.inject(:+)
+#  end
+#end
+#p max_prime
+p answers[-1].inject(:+)
+print "took " + ((Time.now - start) * 1000).round.to_s + "ms.\n"
+
