646c1525c677f6ff6b9555866f59020d05dd2746
hidehigo <hidehigo@gmail.com>
2013-07-29 08:56:50 +0900

    perl TDD: refactor tests

--- tdd/TestDrivenDevelopmentWithPerl/t/TestPercentOwnership.pm
+++ tdd/TestDrivenDevelopmentWithPerl/t/TestPercentOwnership.pm
@@ -5,8 +5,18 @@ use strict;
 use base qw(Test::Class);
 use Test::More;
 use PercentOwnership;
+use List::MoreUtils qw(pairwise);
+
+our ( $a, $b );
 
 __PACKAGE__->runtests unless caller;
+
+sub unit_adder {
+  my ( $self, @args ) = @_;
+  my @arg_names = qw(unit_number square_footage floor);
+  $self->{po}->add_unit(
+    pairwise { ( $a, $b ) } @arg_names, @args );
+}
 
 sub startup_test : Test( startup => 2 ) {
   use_ok( 'PercentOwnership' );
@@ -24,13 +34,7 @@ sub single_unit : Test(2) {
   my ($self) = @_;
   my $po = $self->{po};
 
-  ok(
-    $po->add_unit(
-      unit_number  => 101,
-      square_footage => 450,
-      floor => 1.
-    ), 'added sigle unit'
-  );
+  ok( $self->unit_adder( 101, 450, 1 ), 'added the only unit' );
 
   is( $po->percent_ownership( unit_number => 101 ), 100,
    'single unit condo' );
@@ -40,21 +44,8 @@ sub two_units : Test(4) {
   my ($self) = @_;
   my $po = $self->{po};
 
-  ok(
-    $po->add_unit(
-      unit_number  => 101,
-      square_footage => 450,
-      floor => 1.
-    ), 'added first unit'
-  );
-
-  ok(
-    $po->add_unit(
-      unit_number  => 102,
-      square_footage => 450,
-      floor => 1.
-    ), 'added second unit'
-  );
+  ok( $self->unit_adder( 101, 450, 1 ), 'added the first unit' );
+  ok( $self->unit_adder( 102, 450, 1 ), 'added the second unit' );
 
   is( $po->percent_ownership( unit_number => 101 ), 50,
    'unit 101 got the correct percentage' );
