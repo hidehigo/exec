7297608eea5400122890dc2e449c91373bca1bd8
hidehigo <hidehigo@gmail.com>
2013-07-29 08:34:33 +0900

    perl TDD: add tests

--- tdd/TestDrivenDevelopmentWithPerl/t/TestPercentOwnership.pm
+++ tdd/TestDrivenDevelopmentWithPerl/t/TestPercentOwnership.pm
@@ -32,5 +32,33 @@ sub single_unit : Test(4) {
    'single unit condo' );
 }
 
+sub two_units : Test(6) {
+  my ($self) = @_;
+  my $po = PercentOwnership->new();
+  isa_ok( $po, 'PercentOwnership' );
+  can_ok( 'PercentOwnership', qw( add_unit percent_ownership ) );
+
+  ok(
+    $po->add_unit(
+      unit_number  => 101,
+      square_footage => 450,
+      floor => 1.
+    ), 'added first unit'
+  );
+
+  ok(
+    $po->add_unit(
+      unit_number  => 102,
+      square_footage => 450,
+      floor => 1.
+    ), 'added second unit'
+  );
+
+  is( $po->percent_ownership( unit_number => 101 ), 50,
+   'unit 101 got the correct percentage' );
+  
+  is( $po->percent_ownership( unit_number => 102 ), 50,
+   'unit 102 got the correct percentage' );
+}
 1;
 
