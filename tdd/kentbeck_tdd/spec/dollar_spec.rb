1a25e7bbfd8c63adc6108beec862462a61dc747a
hidehigo <hidehigo@gmail.com>
2013-10-25 08:48:46 +0900

    red : scope変更。これで意図したテストに

--- tdd/kentbeck_tdd/spec/dollar_spec.rb
+++ tdd/kentbeck_tdd/spec/dollar_spec.rb
@@ -2,20 +2,20 @@
 
 require 'dollar'
 describe Dollar, "#times" do
-  before { @five = Dollar.new(5) }
+  five = Dollar.new(5)
   describe "instantiation" do
-    it { expect(@five).not_to be_nil }
+    it { expect(five).not_to be_nil }
   end
   context "times twice => amount should be 10" do
     it {
-      @five.times(2)
-      expect(@five.amount).to eq(10)
+      five.times(2)
+      expect(five.amount).to eq(10)
     }
   end
   context "times three => amount should be 15" do
     it {
-      @five.times(3)
-      expect(@five.amount).to eq(15)
+      five.times(3)
+      expect(five.amount).to eq(15)
     }
   end
 end
