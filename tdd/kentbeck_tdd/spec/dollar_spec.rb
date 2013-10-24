c1e8b17c18bacf6205ef81c6300397725bdd9f05
hidehigo <hidehigo@gmail.com>
2013-10-25 08:53:54 +0900

    red : 意図するtestはこっち

--- tdd/kentbeck_tdd/spec/dollar_spec.rb
+++ tdd/kentbeck_tdd/spec/dollar_spec.rb
@@ -2,20 +2,20 @@
 
 require 'dollar'
 describe Dollar, "#times" do
-  five = Dollar.new(5)
+  before { @five = Dollar.new(5) }
   describe "instantiation" do
-    it { expect(five).not_to be_nil }
+    it { expect(@five).not_to be_nil }
   end
   context "times twice => amount should be 10" do
     it {
-      five.times(2)
-      expect(five.amount).to eq(10)
+      product = @five.times(2)
+      expect(product.amount).to eq(10)
     }
   end
   context "times three => amount should be 15" do
     it {
-      five.times(3)
-      expect(five.amount).to eq(15)
+      product = @five.times(3)
+      expect(product.amount).to eq(15)
     }
   end
 end
