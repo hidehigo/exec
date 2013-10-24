e7e0efe5f068b99b3818758cc206b81da7452bba
hidehigo <hidehigo@gmail.com>
2013-10-25 08:23:34 +0900

    green : assertionは2つだったので変更

--- tdd/kentbeck_tdd/spec/dollar_spec.rb
+++ tdd/kentbeck_tdd/spec/dollar_spec.rb
@@ -4,12 +4,12 @@ require 'dollar'
 describe Dollar, "" do
   before { @five = Dollar.new(5) }
   describe "instantiation" do
-    it { @five.should_not be nil }
+    it { @five.should_not be_nil }
   end
-  describe "times twice" do
-    it { @five.times(2) }
-  end
-  describe "amount should be 10" do
-    it { @five.amount.should eq(10) }
+  context "times twice => amount should be 10" do
+    it {
+      @five.times(2)
+      @five.amount.should eq(10)
+    }
   end
 end
