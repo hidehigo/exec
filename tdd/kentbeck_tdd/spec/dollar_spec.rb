6650c4eb7b6f4627bd01a157c76f58768d091b26
hidehigo <hidehigo@gmail.com>
2013-11-06 08:29:40 +0900

    R : add franc tests

--- tdd/kentbeck_tdd/spec/dollar_spec.rb
+++ tdd/kentbeck_tdd/spec/dollar_spec.rb
@@ -27,3 +27,20 @@ describe Dollar, "#equals" do
     it { expect( @five.equals?(Dollar.new(6)) ).to be_false }
   end
 end
+
+describe Franc, "#times" do
+  before { @five = Franc.new(5) }
+  describe "instantiation" do
+    it { expect(@five).not_to be_nil }
+  end
+  context "times twice => amount should be 10" do
+    it {
+      expect(@five.times(2).equals?(Franc.new(10))).to be_true
+    }
+  end
+  context "times three => amount should be 15" do
+    it {
+      expect(@five.times(3).equals?(Franc.new(15))).to be_true
+    }
+  end
+end
