31dd344cc36b5335f21ec80e555a30a503f88f75
hidehigo <hidehigo@gmail.com>
2013-11-13 08:16:05 +0900

    R : francをfactory化

--- tdd/kentbeck_tdd/spec/franc_spec.rb
+++ tdd/kentbeck_tdd/spec/franc_spec.rb
@@ -3,18 +3,18 @@
 require 'franc'
 
 describe Franc, "#times" do
-  before { @five = Franc.new(5) }
+  before { @five = Money.franc(5) }
   describe "instantiation" do
     it { expect(@five).not_to be_nil }
   end
   context "times twice => amount should be 10" do
     it {
-      expect(@five.times(2).equals?(Franc.new(10))).to be_true
+      expect(@five.times(2).equals?(Money.franc(10))).to be_true
     }
   end
   context "times three => amount should be 15" do
     it {
-      expect(@five.times(3).equals?(Franc.new(15))).to be_true
+      expect(@five.times(3).equals?(Money.franc(15))).to be_true
     }
   end
 end
