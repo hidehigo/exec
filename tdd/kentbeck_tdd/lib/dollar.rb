82eaeb8a277ffbb1e87ee4bf9758f207d1f5095d
hidehigo <hidehigo@gmail.com>
2013-11-22 08:29:11 +0900

    G : currency をとりあえず実装

--- tdd/kentbeck_tdd/lib/dollar.rb
+++ tdd/kentbeck_tdd/lib/dollar.rb
@@ -3,4 +3,7 @@ class Dollar < Money
   def times(multiplier)
     return Dollar.new( @amount * multiplier )
   end
+  def currency()
+    return "USD"
+  end
 end
--- tdd/kentbeck_tdd/lib/franc.rb
+++ tdd/kentbeck_tdd/lib/franc.rb
@@ -3,4 +3,7 @@ class Franc < Money
   def times(multiplier)
     return Franc.new( @amount * multiplier )
   end
+  def currency()
+    return "CHF"
+  end
 end
