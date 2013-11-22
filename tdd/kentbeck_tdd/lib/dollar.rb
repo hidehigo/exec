0c55f25ebed0fbcd6e83eabd9a45c255bbe31404
hidehigo <hidehigo@gmail.com>
2013-11-22 18:53:28 +0900

    refactor : Moneyにしても通る

--- tdd/kentbeck_tdd/lib/dollar.rb
+++ tdd/kentbeck_tdd/lib/dollar.rb
@@ -1,6 +1,6 @@
 require 'money'
 class Dollar < Money
   def times(multiplier)
-    return Dollar.new( @amount * multiplier, @currency )
+    return Money.new( @amount * multiplier, @currency )
   end
 end
