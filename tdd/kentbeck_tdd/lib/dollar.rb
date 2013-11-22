253b5983f2940ae047fd2e7865ed2f0b32bddbef
hidehigo <hidehigo@gmail.com>
2013-11-22 18:52:03 +0900

    G : spell miss.. これまでcurrencyは比較してないので気づけなかった。。

--- tdd/kentbeck_tdd/lib/dollar.rb
+++ tdd/kentbeck_tdd/lib/dollar.rb
@@ -1,6 +1,6 @@
 require 'money'
 class Dollar < Money
   def times(multiplier)
-    return Dollar.new( @amount * multiplier, @curerncy )
+    return Dollar.new( @amount * multiplier, @currency )
   end
 end
