59bdf6d374242582fc896b5a5dc707aac4349a04
hidehigo <hidehigo@gmail.com>
2013-11-22 18:40:35 +0900

    R : Franc とMoney の比較

--- tdd/kentbeck_tdd/spec/franc_spec.rb
+++ tdd/kentbeck_tdd/spec/franc_spec.rb
@@ -29,3 +29,8 @@ describe Franc, "#equals" do
   end
 end
 
+describe "test different class equality" do
+  context "compare with Money and Franc" do
+    it { expect( Money.new( 10, "CHF" ) ).to eq( Franc.new( 10, "CHF" ) ) }
+  end
+end
