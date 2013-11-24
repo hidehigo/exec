ef1e83670dfc30614200fc6892423e98980f4214
hidehigo <hidehigo@gmail.com>
2013-11-25 08:13:54 +0900

    refactor : DollarとFrancの削除に向けて Francの==は充分testできてる

--- tdd/kentbeck_tdd/spec/franc_spec.rb
+++ tdd/kentbeck_tdd/spec/franc_spec.rb
@@ -19,16 +19,6 @@ describe Franc, "#times" do
   end
 end
 
-describe Franc, "#equals" do
-  before { @five = Money.franc(5) }
-  context "compare with Franc.new(5)" do
-    it { expect( @five == Money.franc(5) ).to be_true }
-  end
-  context "compare with Franc.new(6)" do
-    it { expect( @five == Money.franc(6) ).to be_false }
-  end
-end
-
 describe "test different class equality" do
   context "compare with Money and Franc" do
     it { expect( Money.new( 10, "CHF" ) ).to eq( Franc.new( 10, "CHF" ) ) }
