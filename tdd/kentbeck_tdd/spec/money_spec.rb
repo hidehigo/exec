9a161f59cc7c7d0c6cf4277b8f2d8df32512c981
hidehigo <hidehigo@gmail.com>
2013-11-27 08:52:18 +0900

    R : Sumのtest

--- tdd/kentbeck_tdd/spec/money_spec.rb
+++ tdd/kentbeck_tdd/spec/money_spec.rb
@@ -50,3 +50,16 @@ describe "#plus" do
   end
 end
 
+describe Sum, "plus returns Sum" do 
+  before {
+    @five = Money.dollar(5)
+    result = @five.plus(@five)
+    @sum = result
+  }
+  context "check augend" do
+    it { expect(@five).to eq(@sum.augend) }
+  end
+  context "check addend" do
+    it { expect(@five).to eq(@sum.addend) }
+  end
+end
