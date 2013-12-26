1c737207699d5d1478441b54ee27a4fc659d5c0f
hidehigo <hidehigo@gmail.com>
2013-12-27 08:28:35 +0900

    R : add test for Sum.times

--- tdd/kentbeck_tdd/spec/sum_spec.rb
+++ tdd/kentbeck_tdd/spec/sum_spec.rb
@@ -26,3 +26,14 @@ describe Sum, "#plus" do
   end
 end
 
+describe Sum, "#times" do
+  context "sum times" do
+    fiveBucks = Money.dollar(5)
+    tenFrancs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum.new(fiveBucks, tenFrancs).times(2)
+    result = bank.reduce(sum, "USD")
+    it { expect(result).to eq(Money.dollar(20)) }
+  end
+end
