b3dbca6d20c7075775bcfb2befbf8f448dc15ab2
hidehigo <hidehigo@gmail.com>
2013-12-27 08:22:55 +0900

    R : added sum test

--- tdd/kentbeck_tdd/spec/sum_spec.rb
+++ tdd/kentbeck_tdd/spec/sum_spec.rb
@@ -14,3 +14,15 @@ describe Sum, "#plus returns Sum" do
   end
 end
 
+describe Sum, "#plus" do
+  context "sum plus money" do
+    fiveBucks = Money.dollar(5)
+    tenFrancs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum.new(fiveBucks, tenFrancs).plus(fiveBucks)
+    result = bank.reduce(sum, "USD")
+    it { expect(result).to eq(Money.dollar(15)) }
+  end
+end
+
