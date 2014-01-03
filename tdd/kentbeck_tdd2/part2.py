3f09a3aa1a9f160bf7ec4eab9db578918150994d
hidehigo <hidehigo@gmail.com>
2014-01-03 09:29:22 +0900

    refactor:

--- tdd/kentbeck_tdd2/part2.py
+++ tdd/kentbeck_tdd2/part2.py
@@ -1,8 +1,9 @@
 class WasRun:
   def __init__(self, name):
     self.wasRun = None
+    self.name = name
   def testMethod(self):
     self.wasRun = 1
   def run(self):
-    self.testMethod()
-
+    method = getattr(self, self.name)
+    method()
