353e88f2d03c566465dbb0eb83d0c6355019f101
hidehigo <hidehigo@gmail.com>
2014-01-03 09:40:45 +0900

    refactor: TestCaseTest sample

--- tdd/kentbeck_tdd2/part2_test.py
+++ tdd/kentbeck_tdd2/part2_test.py
@@ -1,6 +1,16 @@
-from part2 import WasRun
+from part2 import WasRun, TestCase
 
 test = WasRun("testMethod")
 print test.wasRun
 test.run()
 print test.wasRun
+
+class TestCaseTest(TestCase):
+  def testRunning(self):
+    test = WasRun("testMethod")
+    assert(not test.wasRun)
+    test.run()
+    assert(test.wasRun)
+TestCaseTest("testRunning").run()
+
+
