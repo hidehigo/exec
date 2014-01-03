c06ea05a47e3f32ad77fc095b54d4dc6f5b5c71d
hidehigo <hidehigo@gmail.com>
2014-01-03 09:35:33 +0900

    refactor: TestCaseをsuperに

--- tdd/kentbeck_tdd2/part2.py
+++ tdd/kentbeck_tdd2/part2.py
@@ -1,9 +1,14 @@
-class WasRun:
+class TestCase:
   def __init__(self, name):
-    self.wasRun = None
     self.name = name
-  def testMethod(self):
-    self.wasRun = 1
   def run(self):
     method = getattr(self, self.name)
     method()
+
+class WasRun(TestCase):
+  def __init__(self, name):
+    self.wasRun = None
+    TestCase.__init__(self, name)
+  def testMethod(self):
+    self.wasRun = 1
+
