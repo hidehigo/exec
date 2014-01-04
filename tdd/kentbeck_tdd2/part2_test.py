452b190f47c6ef2552e426e7821472ebd78531c1
hidehigo <hidehigo@gmail.com>
2014-01-04 13:54:58 +0900

    R: added test for brokenMethod

--- tdd/kentbeck_tdd2/part2_test.py
+++ tdd/kentbeck_tdd2/part2_test.py
@@ -14,7 +14,12 @@ class TestCaseTest(TestCase):
     test = WasRun("testMethod")
     result = test.run()
     assert("1 run, 0 failed" == result.summary())
+  def testFailedResult(self):
+    test = WasRun("testBrokenMethod")
+    result = test.run()
+    assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").testResult()
+TestCaseTest("testBrokenMethod").testFailedResult()
 
 
