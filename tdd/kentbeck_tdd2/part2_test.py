735eb03d6caad8bd07d5669e3e1f1952193db97e
hidehigo <hidehigo@gmail.com>
2014-01-04 13:30:10 +0900

    R: added test for tearDown

--- tdd/kentbeck_tdd2/part2_test.py
+++ tdd/kentbeck_tdd2/part2_test.py
@@ -9,7 +9,7 @@ class TestCaseTest(TestCase):
   def testTemplateMethod(self):
     test = WasRun("testMethod")
     test.run()
-    assert("setUp testMethod " == test.log)
+    assert("setUp testMethod tearDown " == test.log)
 TestCaseTest("testTemplateMethod").run()
 
 
