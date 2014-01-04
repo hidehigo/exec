d7761297880825631025ab08ea48123537afefb9
hidehigo <hidehigo@gmail.com>
2014-01-04 13:51:20 +0900

    G:

--- tdd/kentbeck_tdd2/part2.py
+++ tdd/kentbeck_tdd2/part2.py
@@ -12,11 +12,13 @@ class TestCase:
   def setUp(self):
     pass
   def run(self):
+    result = TestResult()
+    result.testStarted()
     self.setUp()
     method = getattr(self, self.name)
     method()
     self.tearDown()
-    return TestResult()
+    return result
   def tearDown(self):
     pass
 
