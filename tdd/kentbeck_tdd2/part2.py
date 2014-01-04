8d3b0a602a4e15f256d6d2849e516e9897f0de85
hidehigo <hidehigo@gmail.com>
2014-01-04 13:56:34 +0900

    G?: いったんExceptionを上げるところまで

--- tdd/kentbeck_tdd2/part2.py
+++ tdd/kentbeck_tdd2/part2.py
@@ -29,6 +29,8 @@ class WasRun(TestCase):
   def testMethod(self):
     self.wasRun = 1
     self.log = self.log + "testMethod "
+  def testBrokenMethod(self):
+    raise Exception
   def setUp(self):
     self.wasRun = None
     self.log = "setUp "
