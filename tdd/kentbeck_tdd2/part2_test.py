91a40d43d3ed5b35ccccdb29f9585796464f0af5
hidehigo <hidehigo@gmail.com>
2014-01-04 13:28:27 +0900

    refactor: ログ化完了

--- tdd/kentbeck_tdd2/part2_test.py
+++ tdd/kentbeck_tdd2/part2_test.py
@@ -6,11 +6,10 @@ test.run()
 print test.wasRun
 
 class TestCaseTest(TestCase):
-  def setUp(self):
-    self.test = WasRun("testMethod")
   def testTemplateMethod(self):
-    self.test.run()
-    assert("setUp testMethod " == self.test.log)
+    test = WasRun("testMethod")
+    test.run()
+    assert("setUp testMethod " == test.log)
 TestCaseTest("testTemplateMethod").run()
 
 
